2ccbab9a0e638fbc1901cce90a3015a962e203de
root <root@ip-172-31-83-191.ec2.internal>
2022-09-21 08:06:24 +0000

    files

--- ec2.tf
+++ ec2.tf
@@ -9,21 +9,7 @@ resource "aws_instance" "ec2-public" {
   user_data                   = file("data.sh")
 
   tags = {
-    Name = "EC2-PUBLIC"
+    Name = "ec2-public"
   }
 }
-#Create a new EC2 launch configuration
-resource "aws_instance" "vamsi-cc" {
-  ami                         = "ami-05fa00d4c63e32376"
-  instance_type               = "t2.micro"
-  key_name                    = "key"
-  security_groups             = ["${aws_security_group.tf-sg.id}"]
-  subnet_id                   = aws_subnet.tf-pub-sn-2.id
-  associate_public_ip_address = true
-  user_data                   = file("data1.sh")
 
-
-  tags = {
-    Name = "vamsi-cc"
-  }
-}
